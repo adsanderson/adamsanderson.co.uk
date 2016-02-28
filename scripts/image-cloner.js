452cc921dd6dd28cc5b89a20fcb75145136adb36
Adam Sanderson <lazydayed@gmail.com>
2016-02-28 15:59:09 +0000

    move from parse to AST

--- scripts/image-cloner.js
+++ scripts/image-cloner.js
@@ -1,2 +1,14 @@
-'use strict;
+'use strict';
+
+var fs = require("fs");
+var glob = require("glob");
+var parse = require("markdown-to-ast").parse;
+
+
+
+glob("source/**/*.md", {}, function (er, files) {
+    var content = fs.readFileSync(files[0]);
+    var result = parse(content.toString());
+    console.log(result);
+});
 
