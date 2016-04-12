0372b712cb0f27628df819969c75f1dd0680d732
Adam Sanderson <lazydayed@gmail.com>
2016-04-12 11:01:30 +0000

    final version of dynamic reading lists

--- scripts/getInterestingThings.js
+++ scripts/getInterestingThings.js
@@ -12,19 +12,27 @@ function processPosts (posts) {
 }
 
 function callback(error, response, body) {
+  console.log(`Interesting posts response ${response.statusCode}`);
   if (!error && response.statusCode == 200) {
     var interesting = JSON.parse(body);
-    Promise.all(processPosts(interesting.posts));
+    return Promise.all(processPosts(interesting.posts))
+    .then(() => {
+      console.log('All files written');
+    });
   }
+  console.error(`error response`, error);
 }
 
 function writePost (name, content, resolve, reject) {
-  fs.writeFile(`${name}.md`, content, function(err) {
+  fs.writeFile(`source/_posts/${name}.md`, content, function(err) {
       if(err) {
-          return reject(err);
+        console.error('error write', err);
+        return reject(err);
       }
+      console.log(`file write - ${name}`);
       return resolve();
   });
 }
 
+console.log(`Get interesting posts`);
 request('http://api.adamsanderson.co.uk/interesting', callback);
