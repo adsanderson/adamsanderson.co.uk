3ec7962367fd5f228e5970b15b2f51e64a992afb
Adam Sanderson <lazydayed@gmail.com>
2016-10-12 20:58:24 +0100

    A11y (#25)
    * add tests
    
    * update install process
    
    * reorder build
    
    * initial setup for programmable server
    
    * add test server
    
    * Update theme and markdown renderer
    
    * merge origin master
    
    * should of stashed
    
    * add tests
    
    * update install process
    
    * reorder build
    
    * Revert _variables
    
    * Restore header
    
    * Update to more accessable contrast theme
    
    * Restore build
    
    * Remove test script
    
    * Remove test script

---
+++ _test/phantom.test.js
@@ -0,0 +1,39 @@
+console.log('test test test');
+
+// phantom.exit(0);
+const server = require('./fixtures/server');
+
+console.log('test test test');
+
+server.listen(() => {
+  // setTimeout(() => {
+  //   console.log('timeout')
+  //   server.cancel();
+  // }, 1000);
+});
+
+
+// phantom.exit(0);
+
+// var page = require('webpage').create();
+
+// page.viewportSize = {
+//   width: 480,
+//   height: 800
+// };
+
+// page.onResourceRequested = function(request) {
+//   console.log('Request ' + JSON.stringify(request.url, undefined, 4));
+// };
+// page.onResourceReceived = function(response) {
+//   console.log('Receive ' + JSON.stringify(response.url, undefined, 4));
+// };
+
+// page.open('http://adamsandersoncouk.lazydayed.c9users.io/', function(status) {
+//   console.log("Status: " + status);
+//   // console.log(page.content);
+//   if(status === "success") {
+//     page.render('example.png');
+//   }
+//   phantom.exit();
+// });
